8fd6bc4f1b7cf407316c58e38fce1b7c304c5c4f
Might <omegaclassic.official@gmail.com>
2023-01-22 23:18:44 +0300

    Разрешён конфликт слияния

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Калинин");
+        System.out.println("22ПОИТ1з");
+
+        //System.out.println("Curr dir " + Paths.get(".").toAbsolutePath());
+        List<BankOperation> operationList = FileReader.readFromFile("data.txt");
     }
 }
