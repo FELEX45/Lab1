d23dd068c606d3e455e5921a34ebfa883da57c0d
Might <omegaclassic.official@gmail.com>
2023-01-22 23:12:05 +0300

    Реализовано форматирование данных при выводе в консоль

--- src/BankOperation.java
+++ src/BankOperation.java
@@ -1,2 +1,51 @@
 public class BankOperation {
+
+    private enum TypeOperation{
+        ADD,
+        REMOVE
+    }
+
+    public final int date_day;
+    public final int date_month;
+    public final int date_year;
+
+    public final int time_hour;
+    public final int time_min;
+    public final int time_second;
+
+    public final String type;
+    public final String account;
+
+    public final double amount;
+    public final String description;
+
+    public BankOperation(int date_day, int date_month, int date_year, int time_hour, int time_min, int time_second, String type, String account, double amount, String description) {
+        this.date_day =     Math.abs(date_day);
+        this.date_month =   Math.abs(date_month);
+        this.date_year =    Math.abs(date_year);
+        this.time_hour =    Math.abs(time_hour);
+        this.time_min =     Math.abs(time_min);
+        this.time_second =  Math.abs(time_second);
+
+        this.type = TypeOperation.valueOf(type).name();
+        this.account = account;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "BankOperation{" +
+                "date_day=" + date_day +
+                ", date_month=" + date_month +
+                ", date_year=" + date_year +
+                ", time_hour=" + time_hour +
+                ", time_min=" + time_min +
+                ", time_second=" + time_second +
+                ", type='" + type + '\'' +
+                ", account='" + account + '\'' +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
