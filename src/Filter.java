bd93ee3d29026775329586b2e7e18ac1c2807b32
Might <omegaclassic.official@gmail.com>
2023-01-22 23:31:25 +0300

    Добавлена фильтрация, исправлены исходные данные для фильтрации

---
+++ src/Filter.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Filter {
+    public static List<BankOperation> sortADDOperation(List<BankOperation> input) {
+        List<BankOperation> out = new ArrayList<>();
+        for (BankOperation bankOperation : input) {
+            if (bankOperation.type.equals(BankOperation.TypeOperation.ADD.name())) {
+                out.add(bankOperation);
+            }
+        }
+        return out;
+    }
+
+    public static List<BankOperation> sortNov2021(List<BankOperation> input) {
+        List<BankOperation> out = new ArrayList<>();
+        for (BankOperation bankOperation : input) {
+            if (bankOperation.date_year == 2021 && bankOperation.date_month == 10) {
+                out.add(bankOperation);
+            }
+        }
+        return out;
+    }
+}
