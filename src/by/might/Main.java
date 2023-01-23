5317bb62c8541ceb9b1f51de64d5783d45de68ff
Might <omegaclassic.official@gmail.com>
2023-01-23 13:31:44 +0300

    Создание отдельного класса для выполнения фильтрации с сортировок

--- src/by/might/Main.java
+++ src/by/might/Main.java
@@ -1,34 +1,15 @@
 package by.might;
 
+import org.magiclen.magicsort.MagicSort;
+
 import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Калинин");
         System.out.println("22ПОИТ1з");
 
-        //System.out.println("Curr dir " + Paths.get(".").toAbsolutePath());
-        List<BankOperation> operationList = FileReader.readFromFile("data.txt");
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Фильтрация данных");
-        System.out.println("1. Вывести все банковские приходные операции");
-        System.out.println("2. Вывести все банковские операции за ноябрь 2021 года.");
-        System.out.println("Выберите операцию для фильтрации");
-
-        int read = scanner.nextInt();
-        if (read == 1) {
-            Filter.sortADDOperation(operationList).forEach(System.out::println);
-        }
-        else if (read == 2) {
-            Filter.sortNov2021(operationList).forEach(System.out::println);
-        }
-
-        System.out.println("Сортируем");
-        Collections.sort(operationList, new Sort.BankOperationDescUpComparator());
-        operationList.forEach(System.out::println);
+        new Process().run();
     }
 }
---
+++ src/by/might/Process.java
@@ -0,0 +1,52 @@
+package by.might;
+
+import org.magiclen.magicsort.MagicSort;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Process implements Runnable {
+
+    @Override
+    public void run() {
+        //System.out.println("Curr dir " + Paths.get(".").toAbsolutePath());
+        List<BankOperation> operationList = null;
+        try {
+            operationList = FileReader.readFromFile("data.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Фильтрация данных");
+        System.out.println("1. Вывести все банковские приходные операции");
+        System.out.println("2. Вывести все банковские операции за ноябрь 2021 года.");
+        System.out.println("Выберите операцию для фильтрации");
+
+        int read = scanner.nextInt();
+        if (read == 1) {
+            Filter.sortADDOperation(operationList).forEach(System.out::println);
+        }
+        else if (read == 2) {
+            Filter.sortNov2021(operationList).forEach(System.out::println);
+        }
+
+        System.out.println("Выберите алгоритм сортировки");
+        System.out.println("1. Сортировка выбором (Selection sort)");
+        System.out.println("2. Быстрая сортировка (Quick sort)");
+        read = scanner.nextInt();
+
+        MagicSort.Algorithm sorting = read == 1 ? MagicSort.Algorithm.SELECTIONSORT : MagicSort.Algorithm.QUICKSORT;
+        System.out.println("Выберите Критерии сортировки");
+        System.out.println("1. По возрастанию назначения платежа");
+        System.out.println("По возрастанию номера счёта, а в рамках одного счёта по возрастанию вида операции, а в рамках одного вида операции по убыванию суммы операции");
+        read = scanner.nextInt();
+
+        Comparator<BankOperation> comparator = read == 1 ? new Sort.BankOperationDescUpComparator() : new Sort.BankOperationAccountComparator();
+        System.out.println("Сортируем");
+        Arrays.asList(Sort.sortSelect(operationList, comparator, sorting)).forEach(System.out::println);
+    }
+}
