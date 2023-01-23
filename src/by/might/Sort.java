f65b6a5904fe0ca138b01483840e49f6869a5731
Might <omegaclassic.official@gmail.com>
2023-01-23 13:32:10 +0300

    Обновление входных данных и методов сортировки

--- src/by/might/Sort.java
+++ src/by/might/Sort.java
@@ -1,8 +1,19 @@
 package by.might;
 
+import org.magiclen.magicsort.MagicSort;
+
 import java.util.Comparator;
+import java.util.List;
 
 public class Sort {
+
+    public static BankOperation[] sortSelect(List<BankOperation> operations, Comparator<BankOperation> comparator, MagicSort.Algorithm algorithm) {
+        MagicSort<BankOperation> sort = new MagicSort<>(operations.toArray(new BankOperation[0]));
+        sort.setAlgorithm(algorithm);
+        sort.setComparator(comparator);
+        sort.sort();
+        return sort.getData();
+    }
 
     public static class BankOperationDescUpComparator implements Comparator<BankOperation>{
 
